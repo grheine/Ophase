ac7d44c15d8109abaf58a5c14164adb9d1d7b5a5
AlexanderKaschta <alexander.kaschta@t-online.de>
2020-08-20 10:27:28 +0200

    PDO Implementation instead of mysqli - Created error.php, which will be shown, if an error occurs - Created Database.php, a singleton class, handling PDO instances - Changed action_page.php to use PDO - action_page.php will only insert data, if it is called with POST and the required values from the form. If these parameters aren't passed, you will be redirected to error.php - Removed old code from action_page.php - Added .htaccess to the include directory to allow access from the web only to action_page.php

---
+++ inc/Database.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Project: O-Phasen-Anmeldung (Fachschaft Physik, KIT)
+ * Function: Class managing the pdo connection
+ */
+
+require_once("config.php");
+
+class Database
+{
+    private $is_connected = false;
+    private $pdo;
+
+    public function connect(){
+        if ($this->is_connected && !is_null($this->pdo)){
+            return $this->pdo;
+        } else{
+            $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
+            try{
+                $this->pdo = new PDO($dsn, DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+                $this->is_connected = true;
+                return $this->pdo;
+            }catch (PDOException $ex){
+                return null;
+            }
+
+        }
+    }
+
+}
