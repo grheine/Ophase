c5229f2ad3e41e7cd03e6d8a9a237aa7c27634f0
greta <greta.heine@gmx.de>
2020-10-23 16:56:11 +0200

    broken links gefixt

--- kummerkasten_orga.php
+++ kummerkasten_orga.php
@@ -7,7 +7,7 @@
     <div class="wrap-contact">
 
       <?php 
-      $title = "Organizational Questions";
+      $title = "Organisatorische Fragen";
       include('banner.php'); 
       ?>
 
@@ -15,9 +15,9 @@
       <form class="validate-form" action="<?= dirname($_SERVER['PHP_SELF'])?>/inc/send_feedback_orga.php" method="post">
         <div class="wide">
 
-          <h2> Organizational questions and feedback </h2>
-           <p> You have organizational questions regarding the O-phase and the <a href="<?= dirname($_SERVER['PHP_SELF'])?>/faq">FAQ</a> couldn't help you further? Then you can send a message to the organization here. <br>
-           If you have any other problems that you would like to share confidentially, our confidants are at your side and will take care of your concerns. Just click <a href="<?= dirname($_SERVER['PHP_SELF'])?>/kummerkasten_privat">here</a> and describe your request.
+          <h2> Organisatorische Fragen und Feedback  </h2>
+           <p> Du hast organisatorische Fragen oder Probleme und das <a href="<?= dirname($_SERVER['PHP_SELF'])?>/faq">FAQ</a> konnte dir nicht helfen? Dann kannst du hier eine Nachricht an das Orga Team senden <br>
+           Wenn du persönliche Probleme bezüglich der O-Phase hast oder dich aus irgendeinem Grund unwohl fühlst, kannst du unsere Vertrauenspersonen <a href="<?= dirname($_SERVER['PHP_SELF'])?>/kummerkasten_privat">hier</a> erreichen und dein Problem schildern. 
           </p>
         </div>
 
@@ -26,20 +26,20 @@
         <div class="contact-form ">
 
         <div class="wrap-input validate-input" data-validate="fehlt">
-          <span class="label-input">Message:</span>
-          <textarea class="input" name="maintext" placeholder="Your message to the team"></textarea>
+          <span class="label-input">Nachricht:</span>
+          <textarea class="input" name="maintext" placeholder="Deine Nachricht "></textarea>
           <span class="focus-input"></span>
         </div>  
 
         <div class="wrap-input validate-input" data-validate="fehlt">
           <span class="label-input">Name:</span>
-          <input class="input" type="text" name="name"  placeholder="Please insert your name" >
+          <input class="input" type="text" name="name"  placeholder="Bitte Name eintragen" >
           <span class="focus-input"></span>
         </div>
 
         <div class="wrap-input validate-input" data-validate="fehlt">
-          <span class="label-input">Email:</span>
-          <input class="input" type="text" name="email" placeholder="Please insert your email">
+          <span class="label-input">E-Mail:</span>
+          <input class="input" type="text" name="email" placeholder="Bitte Mailadresse eintragen">
           <span class="focus-input"></span>
         </div>
 
@@ -48,7 +48,7 @@
         <div class="wide-check">
           <div class="question-answer question input validate-input " data-validate = "fehlt" >
             <input type="radio" class="input" value="datenschutz" id="datenschutz" name="datenschutz"/>
-            <label for="datenschutz" class="check"><span class="check"> Herewith, I confirm the correctness of my entered data and I confirm the storing of those (also see <a href="https://fachschaft.physik.kit.edu/drupal/content/datenschutzerklärung">privacy informations</a>)</span></label>
+            <label for="datenschutz" class="check"><span class="check"> Hiermit bestätige ich die Speicherung meiner Daten (siehe auch  <a href="https://fachschaft.physik.kit.edu/drupal/content/datenschutzerklärung">Datenschutzerklärung</a>)</span></label>
           </div>
           <span class="focus-input"></span>
         
@@ -56,7 +56,7 @@
         <div class="container-contact-form-btn">
           <button class="contact-form-btn">
             <span>
-              Submit
+              Abschicken
               <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
             </span>
           </button>
--- kummerkasten_privat.php
+++ kummerkasten_privat.php
@@ -2,7 +2,7 @@
 
   <div class="container-contact">
     <div class="fullwidth">
-      <div class="language"><a href="">de</a><p>|</p><a href="<?= dirname($_SERVER['PHP_SELF'])?>/english/kummerkasten">en</a></div>
+      <div class="language"><a href="">de</a><p>|</p><a href="<?= dirname($_SERVER['PHP_SELF'])?>/english/kummerkasten_privat">en</a></div>
     </div>
     <div class="wrap-contact">
 
